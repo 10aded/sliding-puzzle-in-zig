494c4aa0b307a59b8cef8ea83b5a528ac2e0cc1a
10aded <10aded-Streaming@protonmail.com>
2025-02-01 17:09:30 +1000

    saved simple stdin example

---
+++ io-example.zig
@@ -0,0 +1,12 @@
+const std = @import("std");
+
+pub fn main() !void {
+    const stdout = std.io.getStdOut().writer();
+    const stdin = std.io.getStdIn().reader();
+    
+    try stdout.writeAll("Type your name\n");
+    var buffer: [20]u8 = undefined;
+    @memset(buffer[0..], 0);
+    _ = try stdin.readUntilDelimiterOrEof(buffer[0..], '\n');
+    try stdout.print("Your name is: {s}\n", .{buffer});
+}
