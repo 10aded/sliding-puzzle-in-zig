df2495505373d39159240f65500bbd25c852ff5c
10aded <10aded-Streaming@protonmail.com>
2025-02-10 13:43:43 +1000

    significantly cleaned up fisher-yates code

--- fisher-yates.zig
+++ fisher-yates.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 
-// "Extremely Crude O(n^2) Fisher-Yates and PRNG.
+// "Extremely Crude O(n) Fisher-Yates and PRNG.
 //
 // Basically a sketch / draft.
 //
@@ -12,73 +12,87 @@ const std = @import("std");
 // Globals
 const dprint = std.debug.print;
 
-fn fisher_yates(comptime N : u8, olist : [N] u8) [N] u8 {
-    var list1 = olist;
-    var list2 : [N] u8 = undefined;
-    
+var global_prng : XorShiftPRNG = undefined;
+
+// General idea: construct list2 by picking with uniformly
+// random distribution items from list1, and then removing them.
+//
+// NOTE: (Obvious) Picking uniformly randomly from the ordered set
+// {1, 2, 3, 4, 5} is the same as picking uniformly from
+// {5, 4, 3, 2, 1} ... or any other permutation.
+// As such, the algorithm can be condensed into a single list
+// by swapping.
+
+fn fisher_yates(comptime N : u8, original_list : [N] u8) [N] u8 {
+    var list = original_list;
     for (0..N) |i| {
+        // Pick an random index from 0..N-i;
         const ii : u8 = @intCast(i);
-        const rint = get_randomish_byte_up_to(N - ii);
-        dprint("rint: {}\n", .{rint}); //@debug
-        const relem = list1[rint];
-        list2[i] = relem;
-        for (rint..N-ii-1) |j| {
-            list1[j] = list1[j + 1];
-        }
+        const back_index   = N - 1 - ii;
+        const random_index = get_randomish_byte_up_to(N - ii);
+
+        // Perform the swap.
+        const random_element = list[random_index];
+        const back_element   = list[back_index];
+        list[back_index]     = random_element;
+        list[random_index]   = back_element;
     }
-    return list2;
+    return list;
 }
 
 pub fn main() void {
+    global_prng = initialize_xorshiftprng();
+
     const listA : [5] u8 = .{1,2,3,4,5};
     const listB = fisher_yates(5, listA);
     const listC = fisher_yates(5, listA);
     const listD = fisher_yates(5, listA);
-    dprint("lists:\n{any}\n{any}\n{any}\n", .{listB, listC, listD}); //@debug
-    
+       dprint("lists:\n{any}\n{any}\n{any}\n", .{listB, listC, listD}); //@debug
 }
 
-// Note: Repeated calls of this fn, if using a timestamp will
-// not be very random! As such we pass it through xorshift many times.
-fn get_randomish_byte() u8 {
-    const nano_timestamp : i128 = std.time.microTimestamp();
-    const nano_truncated : i64  = @truncate(nano_timestamp);
-    const converted_timestamp : u64 = @bitCast(nano_truncated);
-
-    var xor_input = converted_timestamp;
-    const XORSHIFT_NUMBER = 10;
-    for (0..XORSHIFT_NUMBER) |i| {
-        _ = i;
-        xor_input = xorshift(xor_input);
-    }
-
-    // Choose the last byte.
-    const rbyte : u8 = @truncate(xor_input);
-    return rbyte;
+fn get_randomish_byte( prng : *XorShiftPRNG) u8 {
+    // Pick a byte near the 'middle'.
+    const byte : u8 = @truncate(prng.state >> 32);
+    prng.update_state();
+    return byte;
 }
 
 // Return a random number from 0..<limit <= 255.
 fn get_randomish_byte_up_to(limit : u8) u8 {
     const remainder = 255 % limit;
-    const okay_range_limit = (255 - remainder) - 1;
-    var rbyte : u8 = 0;
+    const modulo_limit = (255 - remainder) - 1;
+    var random_byte : u8 = 0;
     while (true) {
-        rbyte = get_randomish_byte();
-        if (rbyte <= okay_range_limit) {
+        random_byte = get_randomish_byte(&global_prng);
+        if (random_byte <= modulo_limit) {
             break;
         }
     }
-    const rand = rbyte % limit;
-    return rand;
+    const final_byte = random_byte % limit;
+    return final_byte;
 }
 
-// Eg. 0..=10 for limit = 2 should be 0..=9, since otherwise more even than odd!
+const XorShiftPRNG = struct {
+    state : u64,
 
+    fn update_state(self : *XorShiftPRNG) void {
+        const x1 = self.state; 
+        const x2 = x1 ^ (x1 << 13);
+        const x3 = x2 ^ (x2 >> 7);
+        const x4 = x3 ^ (x3 << 17);
+        self.state = x4;
+    }
+};
 
-// Xorshift example
-fn xorshift( x1 : u64) u64 {
-    const x2 = x1 ^ (x1 << 13);
-    const x3 = x2 ^ (x2 >> 7);
-    const x4 = x3 ^ (x3 << 17);
-    return x4;
+fn initialize_xorshiftprng() XorShiftPRNG {
+    const nano_timestamp : i128 = std.time.microTimestamp();
+    const nano_truncated : i64  = @truncate(nano_timestamp);
+    const converted_timestamp : u64 = @bitCast(nano_truncated);
+
+    var xorshiftprng = XorShiftPRNG{ .state = converted_timestamp };
+    for (0..10) |i| {
+        _ = i;
+        xorshiftprng.update_state();
+    }
+    return xorshiftprng;
 }
